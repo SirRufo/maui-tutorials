ddf5a24488078f7e7b7442b9109d31041a7b7ca9
Oliver Münzberg <oliver.muenzberg@gmail.de>
2023-12-14 17:23:18 +0100

    added RouteNavigation with attached property for Button and MenuItem

---
+++ Navigation/Shared/RouteNavigation.cs
@@ -0,0 +1,42 @@
+﻿namespace Navigation.Shared
+{
+    public static class RouteNavigation
+    {
+        public static string GetRoute( BindableObject target ) => (string)target.GetValue( RouteProperty );
+        public static void SetRoute( BindableObject target, string value ) => target.SetValue( RouteProperty, value );
+
+        public static readonly BindableProperty RouteProperty = BindableProperty.Create( "Route", typeof( string ), typeof( RouteNavigation ), null, propertyChanged: OnRoutePropertyChanged );
+
+        private static void OnRoutePropertyChanged( BindableObject bindable, object oldValue, object newValue )
+        {
+            if ( bindable is Button button )
+            {
+                if ( oldValue is not null && newValue is null )
+                {
+                    button.Clicked -= Item_Clicked;
+                }
+                if ( newValue is not null && oldValue is null )
+                {
+                    button.Clicked += Item_Clicked;
+                }
+            }
+            if ( bindable is MenuItem menuitem )
+            {
+                if ( oldValue is not null && newValue is null )
+                {
+                    menuitem.Clicked -= Item_Clicked;
+                }
+                if ( newValue is not null && oldValue is null )
+                {
+                    menuitem.Clicked += Item_Clicked;
+                }
+            }
+        }
+
+        private static async void Item_Clicked( object sender, EventArgs e )
+        {
+            await Shell.Current.GoToAsync( GetRoute( (BindableObject)sender! ) );
+        }
+    }
+
+}
